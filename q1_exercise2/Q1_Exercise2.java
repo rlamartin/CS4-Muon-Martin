6550e58547a52a93eeeafc72a6a77d9b08f79897
rlamartin <92861184+rlamartin@users.noreply.github.com>
2022-09-06 08:59:58 +0800

    Add files via upload

---
+++ q1_exercise2/Q1_Exercise2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise2;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Exercise2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Rael Martin";
+        int age = 15;
+        boolean isHooman = true;
+           
+    }
+    
+}
