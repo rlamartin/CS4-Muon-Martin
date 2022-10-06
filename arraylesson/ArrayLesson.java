4d51184a6ce314963fc99e0f5518ed215bfaf4dd
rlamartin <92861184+rlamartin@users.noreply.github.com>
2022-10-06 11:26:06 +0800

    Add files via upload

---
+++ arraylesson/ArrayLesson.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylesson;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MUON
+ */
+public class ArrayLesson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // Student mark = new Student("Mark", 12);
+        int [] listOfNum = {1,2,3,4,5};
+        double [] listOfDecimals = new double[4];
+        //this is primitive. primitive good for list where limit not known.
+        listOfDecimals[0] = 1.2;
+        listOfDecimals[1] = 2.3;
+        String [][] listofstrings = new String [2][2];
+        
+        ArrayList<Integer> pair = new ArrayList();
+        //pair.add(mark);
+        
+    }
+    
+}
